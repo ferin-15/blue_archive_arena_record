ece2644bd1e07f1bfbd57c340d3034bb5932f0d7
ferin-15 <ys236236@gmail.com>
2024-11-17 19:59:54 +0900

    コードの整理

--- discord_bot.py
+++ discord_bot.py
@@ -29,55 +29,61 @@ async def on_message(message):
     # bot自身のメッセージには反応しない
     if message.author == client.user:
         return
+
+    # 対象外のチャンネルの場合反応しない    
+    if message.channel.id != channel_id:
+        return
+    
+    # 画像が添付されていない場合反応しない
+    if len(message.attachments) == 0:
+        return
     
     # 結果記録用のチャンネルのID
     channel_id = int(os.getenv('DISCORD_CHANNEL_ID'))
 
-    if message.channel.id == channel_id:
-        # 画像が添付されているかチェック
-        if len(message.attachments) > 0:
-            result_list = []
-            for index, attachment in enumerate(message.attachments, start=1):
-                if attachment.content_type.startswith("image"):
-                    # 画像のダウンロード
-                    image_data = await attachment.read()
-                    # 一時ファイルとして保存
-                    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-                        temp_file.write(image_data)
-                    save_file_path = temp_file.name
+    # 画像が添付されているかチェック
+    result_list = []
+    for index, attachment in enumerate(message.attachments, start=1):
+        if not attachment.content_type.startswith("image"):
+            continue
 
-                    try:
-                        result = recognize_arena_result.recognize_image(save_file_path)
-                        
-                        is_atk = '攻撃' if result['is_atk'] else '防御'
-                        is_win = '勝利' if result['is_win'] else '敗北'
-                        character_list = result['character_list']
+        # 画像のダウンロード
+        image_data = await attachment.read()
+        # 一時ファイルとして保存
+        with tempfile.NamedTemporaryFile() as temp_file:
+            temp_file.write(image_data)
+            save_file_path = temp_file.name
+            try:
+                result = recognize_arena_result.recognize_image(save_file_path)
+            except Exception as e:
+                await message.channel.send(f'Error: {index} 枚目の画像で認識失敗, {e}')
+                continue
 
-                        # 攻撃側が先頭に来るように並び替え
-                        if not result['is_atk']:
-                            character_list = character_list[6:] + character_list[:6]
+        # 攻撃側が先頭に来るように並び替え
+        if not result['is_atk']:
+            character_list = character_list[6:] + character_list[:6]
 
-                        # 攻撃側が勝利
-                        if (is_atk and is_win) or (not is_atk and not is_win):
-                            await message.channel.send(
-                                f'攻撃側勝利\n{character_list}'
-                            )
-                        # 防御側が勝利
-                        else:
-                            await message.channel.send(
-                                f'防御側勝利\n{character_list}'
-                            )
+        is_atk = '攻撃' if result['is_atk'] else '防御'
+        is_win = '勝利' if result['is_win'] else '敗北'
+        character_list = result['character_list']
 
-                    except Exception as e:
-                        await message.channel.send(f'Error: {index} 枚目の画像で認識失敗, {e}')
-                        continue
-                    
-                    if os.getenv('ENV') == 'private':
-                        result_list.append(result)
-            
-            if len(result_list) > 0 and os.getenv('ENV') == 'private':
-                upload_to_spreadsheet.upload_result(result_list)
-                await message.channel.send('結果をアップロードしました')
+        # 攻撃側が勝利
+        if (is_atk and is_win) or (not is_atk and not is_win):
+            await message.channel.send(
+                f'攻撃側勝利\n{character_list}'
+            )
+        # 防御側が勝利
+        else:
+            await message.channel.send(
+                f'防御側勝利\n{character_list}'
+            )
+        
+        if os.getenv('ENV') == 'private':
+            result_list.append(result)
+        
+    if len(result_list) > 0 and os.getenv('ENV') == 'private':
+        upload_to_spreadsheet.upload_result(result_list)
+        await message.channel.send('結果をアップロードしました')
 
 
 client.run(os.getenv('DISCORD_BOT_TOKEN'))
